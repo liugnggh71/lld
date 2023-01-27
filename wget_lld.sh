6b95cfd170da9d77d93d5bbfd383bffd10f71cba
liugnggh71 <liugng71@gmail.com>
2023-01-27 01:07:15 -0600

    update

--- wget_lld.sh
+++ wget_lld.sh
@@ -2,8 +2,10 @@
 pwd=$(pwd)
 cd ${pwd}
 mkdir -p dba_code/bin
-echo -n PATH=${pwd}/dba_code/bin > profile.txt
+echo -n export PATH=${pwd}/dba_code/bin > profile.txt
 echo ':${PATH}' >> profile.txt 
+echo cd dba_code/bin > cd_dba_code_bin.sh
+echo ln -s cd_dba_code_bin.sh BN > ln_bn.sh
 cd ${pwd}/dba_code/bin
 wget https://github.com/liugnggh71/lld/raw/main/dba_code/bin/lld
 chmod u+x lld
