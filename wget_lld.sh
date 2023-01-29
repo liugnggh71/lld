b17e350a7d5045006d20ebb772f9f860658794ec
liugnggh71 <liugng71@gmail.com>
2023-01-29 08:26:07 -0600

    Add updates

--- wget_lld.sh
+++ wget_lld.sh
@@ -22,7 +22,14 @@ wget https://github.com/liugnggh71/lld/raw/main/dba_code/bin/SAX
 chmod u+x SAX
 wget https://github.com/liugnggh71/lld/raw/main/dba_code/bin/SAXON
 chmod u+x SAXON
-wget https://github.com/liugnggh71/lld/raw/main/dba_code/bin/XXX
+cd ${pwd}
+mkdir -p dba_code1/bin
+echo -n export PATH=\${HOME}/dba_code1/bin > profile.txt
+echo ':${PATH}' >> profile.txt 
+echo cd dba_code1/bin > cd_dba_code_bin.sh
+echo ln -s cd_dba_code_bin.sh BN > ln_bn.sh
+cd ${pwd}/dba_code1/bin
+wget https://github.com/liugnggh71/lld/raw/main/dba_code1/bin/XXX
 chmod u+x XXX
 cd ${pwd}/dba_code/bin
 ln -s lld D
